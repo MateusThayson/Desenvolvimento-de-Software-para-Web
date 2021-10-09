a92bfd34b6ccde0b79df7dd50bb14b428ea20ae9
Mateus Thayson <mateusthayson05@gmail.com>
2021-10-08 21:12:52 -0300

    adicionando atividade 01

---
+++ Atividade 01/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { Mateus } from './components/Mateus';
+
+function App() {
+  return (
+        <Mateus></Mateus>
+  );
+}
+
+export default App;
---
+++ Atividade 01/src/components/Mateus.js
@@ -0,0 +1,11 @@
+export default function Mateus(){
+    return(
+        <div>
+            <h3>Mateus Thayson de Sousa</h3><br/>
+            <h4>Idade: 24 anos</h4><br/>
+            <h4>Curso: Design Digital</h4><br/>
+            <h4>Cidade de Origem: Quixadá-Ce</h4><br/>
+            <h4>E-mail: mateusthayson05@gmail.com</h4>
+        </div>
+    )
+}
